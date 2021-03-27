9c0456f41a87f06dcec89ad309e14d3b2e792e36
Travis Lindsey <reachjtl@gmail.com>
2021-03-27 11:01:16 -0400

    added dotenv to show still broken when using dotenv

--- src-ssr/extension.js
+++ src-ssr/extension.js
@@ -10,7 +10,7 @@
  * Note: Changes to this file (but not any file it imports!) are picked up by the
  * development server, but such updates are costly since the dev-server needs a reboot.
  */
-
+require('dotenv').config()
 const v = process.env.VAL
 
 module.exports.extendApp = function ({ app, ssr }) {
